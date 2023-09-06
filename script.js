81f1b39bf68c414db4656c506eb644017273d708
ray-sarthak <117006899+ray-sarthak@users.noreply.github.com>
2023-09-06 23:19:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
 //your JS code here. If required.
+function star(){
+let today=new Date();
+let h=today.getHours();
+let m = today.getMinutes();
+  let s = today.getSeconds();
+	m=checkTime(m);
+	s=checkTime(s);
+  
+let day = today.getDate();
+let month = today.getMonth() + 1;
+let year = today.getFullYear();
+	let anot="AM";
+	if(h>12){
+		h=h-12;
+		anot="PM"
+	}
+	document.getElementById("timer").innerText=month+"/"+day+"/"+year+", "+h+":"+m+":"+s+` ${anot}`;
+	setTimeout(star, 1000);
+	
+}
+function checkTime(i) {
+  if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+  return i;
+}
+
+
+
+
+
+
